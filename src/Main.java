64b83bf5416b4b8061e1b7ac75da27b4e5e68d28
kaio <kaio22barra@gmail.com>
2025-09-01 18:20:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
+/*
+5. Crie a classe Pessoa com:
+• Atributos privados: nome (String),
+idade (int)
+• Um construtor que receba nome e idade
+• Métodos getNome() e getIdade()
+Crie a classe Endereco com:
+• Atributos privados: rua (String),
+numero (int)
+• Um construtor que receba rua e número
+• Métodos getRua() e getNumero()
+POO 31
+Associe as classes:
+• Adicione na classe Pessoa um atributo do
+tipo Endereco chamado endereco
+• Altere o construtor de Pessoa para também
+receber um Endereco
+• Crie um método mostrarDados() que
+exiba o nome, idade e o endereço da pessoa
+No main:
+• Crie dois objetos Endereco
+• Crie duas pessoas, cada uma com seu
+endereço
+• Mostre os dados de cada pessoa
+*/
 public class Main {
     public static void main(String[] args) {
         Endereco end1 = new Endereco("Rua das Flores", 123);
