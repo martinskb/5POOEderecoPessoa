eabc82554a5fb55d5ddd46a9915981589df3784f
kaio <kaio22barra@gmail.com>
2025-09-01 18:02:10 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private Endereco endereco;
+
+    public Pessoa(String nome, int idade, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void mostrarDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Endereço: " + endereco.getRua() + ", " + endereco.getNumero());
+    }
 }
